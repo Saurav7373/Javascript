4b7a5ca552982de39cff885d928bbeac8f417a77
saurav7373 <dreamsaurav7373@gmail.com>
2020-08-13 16:15:05 +0545

    js basics

---
+++ js/basics.js
@@ -0,0 +1,32 @@
+console.log('home')
+// variable declare and is glocal
+var name='saurav'; 
+// var assign
+var mm;
+// intger
+var marks = 34;
+ console.log(name, mm , marks)
+// use const for avoiding overwrite
+const nmaeq = 'Same';
+console.log(nmaeq);
+//  we haven't right to initilizes the const variables
+
+// let is block level initilized 
+{
+    let nmaeq ="haru";
+    // we can overwrite the value of nmaeq by 
+    nmaeq = 'ktm'
+    console.log(nmaeq);
+}
+// array making
+
+const arrq = [2,4,5,6,];
+console.log(arrq);
+
+/*
+camelCase = first letter of any word is samll and others start letter is capital
+kebab-case = same as (kebab-case) word structure
+snake_case
+PascalCase
+*/
+
