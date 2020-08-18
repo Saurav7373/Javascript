3173f6c204d3e1a95757ef03a42f553ff5c77890
saurav7373 <dreamsaurav7373@gmail.com>
2020-08-18 13:54:41 +0545

    Basics JS windows documents

---
+++ js/windowob.js
@@ -0,0 +1,12 @@
+console.log('This is window document practice');
+let a = window.document;
+alert('sau');
+// a = prompt('This will destroy your computer. Type your name');
+
+// a = confirm('Are you sure you want to delete this page?');
+a = window.innerHeight;
+a = innerWidth;
+a = scrollY;
+a = location.toString();
+a = window.history;
+console.log(a);          
